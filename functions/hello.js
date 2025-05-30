2c1e644646d1dc3d5d2d347e2e7c94b30bdd19aa
openjsw <toewpq@gmail.com>
2025-05-30 19:44:45 +0800

    Delete functions/hello.js

--- functions/hello.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  runtime: 'edge',
-};
-
-export async function onRequest(context) {
-  return new Response('hello,api', {
-    headers: { 'content-type': 'text/plain' }
-  });
-}
